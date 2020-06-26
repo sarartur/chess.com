6bb480b6a37993b1991fc2311296a772913fc0fb
saradzhyanartur <saradzhyanartur@gmail.com>
2020-06-26 15:43:06 -0400

    minor comment and syntax chnages

--- chessdotcom/caller.py
+++ chessdotcom/caller.py
@@ -7,20 +7,21 @@ from chessdotcom.errors import ChessDotComError
 
 
 class _internal:
+    """This class holds the methods and variables that are module-only.
     """
-    This class holds the methods and variables that are module-only.
-    """
-    _api_base = "https://api.chess.com/pub"
-    _https_requester = PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=where())
+    _base_url = "https://api.chess.com/pub"
+    _https = PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=where())
 
-    @staticmethod
-    def do_get_request(path):
+    @classmethod
+    def do_get_request(cls, path):
         """Preforms a GET request to the chess.com API using the specified path.
 
-        :param path The URL path to use"""
-        r = _internal._https_requester.request(
+        Parameters:
+            path -- path The URL path to use
+        """
+        r = cls._https.request(
             method='GET',
-            url=_internal._api_base + path
+            url=cls._base_url + path
         )
         if r.status != 200:
             raise ChessDotComError(status_code=r.status)
@@ -45,7 +46,7 @@ def get_titled_players(title_abbrev: str) -> Dict:
         title_abbrev -- abbreviation of chess title
     """
 
-    r = _internal.do_get_request(f"/titled/{title_abbrev}")
+    r = _internal.do_get_request(path = f"/titled/{title_abbrev}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -58,7 +59,7 @@ def get_player_stats(username: str) -> Dict:
         username -- username of the player
     """
 
-    r = _internal.do_get_request(f"/player/{username}/stats")
+    r = _internal.do_get_request(path = f"/player/{username}/stats")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -69,7 +70,7 @@ def is_player_online(username: str) -> Dict:
     Parameters:
         username -- username of the player"""
 
-    r = _internal.do_get_request(f"/player/{username}/is-online")
+    r = _internal.do_get_request(path = f"/player/{username}/is-online")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -81,7 +82,7 @@ def get_player_current_games(username: str) -> Dict:
         username -- username of the player
     """
 
-    r = _internal.do_get_request(f"/player/{username}/games")
+    r = _internal.do_get_request(path = f"/player/{username}/games")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -93,7 +94,7 @@ def get_player_current_games_to_move(username: str) -> Dict:
         username -- username of the player
     """
 
-    r = _internal.do_get_request(f"/player/{username}/games/to-move")
+    r = _internal.do_get_request(path = f"/player/{username}/games/to-move")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -104,7 +105,7 @@ def get_player_game_archives(username: str) -> Dict:
     Parameters:
         username -- username of the player
     """
-    r = _internal.do_get_request(f"/player/{username}/games/archives")
+    r = _internal.do_get_request(path = f"/player/{username}/games/archives")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -117,7 +118,7 @@ def get_player_games_by_month(username: str, yyyy: str, mm: str) -> Dict:
         yyyy -- integer: the year (yyyy)
         mm -- integer: the month (mm)
     """
-    r = _internal.do_get_request(f"/player/{username}/games/{yyyy}/{mm}")
+    r = _internal.do_get_request(path = f"/player/{username}/games/{yyyy}/{mm}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -131,7 +132,7 @@ def get_player_games_by_month_pgn(username: str, yyyy: str, mm: str):
         mm -- integer: the month (mm)
     """
 
-    r = _internal.do_get_request(f"/player/{username}/games/{yyyy}/{mm}/pgn")
+    r = _internal.do_get_request(path = f"/player/{username}/games/{yyyy}/{mm}/pgn")
     return r.data
 
 
@@ -142,7 +143,7 @@ def get_player_clubs(username: str) -> Dict:
     Parameters:
         username -- username of the player
     """
-    r = _internal.do_get_request(f"/player/{username}/clubs")
+    r = _internal.do_get_request(path = f"/player/{username}/clubs")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -153,7 +154,7 @@ def get_player_team_matches(username: str) -> Dict:
     Parameters:
         username -- username of the player
     """
-    r = _internal.do_get_request(f"/player/{username}/matches")
+    r = _internal.do_get_request(path = f"/player/{username}/matches")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -165,7 +166,7 @@ def get_player_tournaments(username: str) -> Dict:
         username -- username of the player
     """
 
-    r = _internal.do_get_request(f"/player/{username}/tournaments")
+    r = _internal.do_get_request(path = f"/player/{username}/tournaments")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -175,7 +176,7 @@ def get_club_details(url_id: str) -> Dict:
     Parameters:
         url_id -- URL for the club's web page on www.chess.com.
     """
-    r = _internal.do_get_request(f"/club/{url_id}")
+    r = _internal.do_get_request(path = f"/club/{url_id}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -185,7 +186,7 @@ def get_club_members(url_id: str) -> Dict:
     Parameters:
         url_id -- URL for the club's web page on www.chess.com.
     """
-    r = _internal.do_get_request(f"/club/{url_id}/members")
+    r = _internal.do_get_request(path = f"/club/{url_id}/members")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -195,7 +196,7 @@ def get_club_matches(url_id: str) -> Dict:
     Parameters:
         url_id -- URL for the club's web page on www.chess.com.
     """
-    r = _internal.do_get_request(f"/club/{url_id}/matches")
+    r = _internal.do_get_request(path = f"/club/{url_id}/matches")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -206,7 +207,7 @@ def get_tournament_details(url_id: str) -> Dict:
     Parameters:
         url_id -- URL for the club's web page on www.chess.com.
     """
-    r = _internal.do_get_request(f"/tournament/{url_id}")
+    r = _internal.do_get_request(path = f"/tournament/{url_id}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -218,7 +219,7 @@ def get_tournament_round(url_id: str, round_num: int) -> Dict:
         url_id -- URL for the club's web page on www.chess.com.
         round_num -- the round of the tournament
     """
-    r = _internal.do_get_request(f"/tournament/{url_id}/{round_num}")
+    r = _internal.do_get_request(path = f"/tournament/{url_id}/{round_num}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -231,7 +232,7 @@ def get_tournament_round_group_details(url_id: str, round_num: int, group_num: i
         round_num -- the round of the tournament
         group_num -- the group in the tournament
     """
-    r = _internal.do_get_request(f"/tournament/{url_id}/{round_num}/{group_num}")
+    r = _internal.do_get_request(path = f"/tournament/{url_id}/{round_num}/{group_num}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -242,7 +243,7 @@ def get_team_match(match_id: int) -> Dict:
     Parameters:
         match_id -- the id of the match
     """
-    r = _internal.do_get_request(f"/match/{match_id}")
+    r = _internal.do_get_request(path = f"/match/{match_id}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -254,7 +255,7 @@ def get_team_match_board(match_id: int, board_num: int) -> Dict:
         match_id -- the id of the match
         board_num -- the number of the board
     """
-    r = _internal.do_get_request(f"/match/{match_id}/{board_num}")
+    r = _internal.do_get_request(path = f"/match/{match_id}/{board_num}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -265,7 +266,7 @@ def get_team_match_live(match_id: int) -> Dict:
     Parameters:
         match_id -- the id of the match
     """
-    r = _internal.do_get_request(f"/match/live/{match_id}")
+    r = _internal.do_get_request(path = f"/match/live/{match_id}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -276,7 +277,7 @@ def get_team_match_live_board(match_id: int, board_num: int) -> Dict:
         match_id -- the id of the match
         board_num -- the number of the board
     """
-    r = _internal.do_get_request(f"/match/live/{match_id}/{board_num}")
+    r = _internal.do_get_request(path = f"/match/live/{match_id}/{board_num}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -286,7 +287,7 @@ def get_country_details(iso: str) -> Dict:
     Parameters:
         iso -- country's 2-character ISO 3166 code
     """
-    r = _internal.do_get_request(f"/country/{iso}")
+    r = _internal.do_get_request(path = f"/country/{iso}")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -297,7 +298,7 @@ def get_country_players(iso):
     Parameters:
         iso -- country's 2-character ISO 3166 code
     """
-    r = _internal.do_get_request(f"/country/{iso}/players")
+    r = _internal.do_get_request(path = f"/country/{iso}/players")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -308,7 +309,7 @@ def get_country_clubs(iso: str) -> Dict:
     Parameters:
         iso -- country's 2-character ISO 3166 code
     """
-    r = _internal.do_get_request(f"/country/{iso}/clubs")
+    r = _internal.do_get_request(path = f"/country/{iso}/clubs")
     return json.loads(r.data.decode('utf-8'))
 
 
@@ -322,19 +323,19 @@ def get_current_daily_puzzle() -> Dict:
 def get_random_daily_puzzle() -> Dict:
     """Public method that returns information about a
     randomly picked daily puzzle"""
-    r = _internal.do_get_request("/puzzle/random")
+    r = _internal.do_get_request(path = "/puzzle/random")
     return json.loads(r.data.decode('utf-8'))
 
 
 def get_streamers():
     """Public method that returns information
     about Chess.com streamers."""
-    r = _internal.do_get_request("/streamers")
+    r = _internal.do_get_request(path = "/streamers")
     return json.loads(r.data.decode('utf-8'))
 
 
 def get_leaderboards():
     """Public method that returns information about top 50 player
     for daily and live games, tactics and lessons."""
-    r = _internal.do_get_request("/leaderboards")
+    r = _internal.do_get_request(path = "/leaderboards")
     return json.loads(r.data.decode('utf-8'))
