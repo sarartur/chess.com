6883e6496352c72ffa69b459a3eebc1dbcfbfcd6
saradzhyanartur <saradzhyanartur@gmail.com>
2020-11-17 04:23:54 -0500

    added docs

--- chessdotcom/__init__.py
+++ chessdotcom/__init__.py
@@ -1,10 +1,3 @@
-
-__all__ = [
-    "is_player_online", 
-    "get_player_stats"
-]
-
-
 from chessdotcom.errors import ChessDotComError
 from chessdotcom.caller import *
 from chessdotcom.response import ChessDotComResponse
