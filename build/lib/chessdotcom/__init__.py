cf368caf744a7adf648347a17732855beef39607
saradzhyanartur <saradzhyanartur@gmail.com>
2020-11-17 01:14:54 -0500

    readme update

--- build/lib/chessdotcom/__init__.py
+++
@@ -1,3 +0,0 @@
-from chessdotcom.errors import ChessDotComError
-from chessdotcom.caller import *
-from chessdotcom.response_parser import ChessDotComResponse
--- build/lib/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-#Makes relative imports possible
