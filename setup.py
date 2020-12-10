254bc2f4301661b5a77a66b77254383024fa6f02
saradzhyanartur <saradzhyanartur@gmail.com>
2020-12-10 02:10:41 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chess.com", 
-    version="1.2.3",
+    version="1.2.4",
     author="Artur Saradzhyan",
     author_email="saradzhyanartur@gmail.com",
     description="Python Wrapper around Chess.com API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/saradzhyanartur/chessdotcom",
+    url="https://github.com/sarartur/chess.com",
     packages=setuptools.find_packages(),
     install_requires=['urllib3==1.25.9', 'certifi==2020.4.5.1'],
     setup_requires=['urllib3==1.25.9', 'certifi==2020.4.5.1'],
