ca51e27529cf637fd0fd8271a19189bb809d10ad
saradzhyanartur <saradzhyanartur@gmail.com>
2021-03-04 23:58:28 -0500

    1.5.6 -- edited README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chess.com", 
-    version="1.5.5",
+    version="1.5.6",
     author="Artur Saradzhyan",
     author_email="saradzhyanartur@gmail.com",
     description="Python Wrapper around Chess.com API",
