9e91f3662a23a42b4f634795897cace7b30ac5dd
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-10-09 22:45:17 -0400

    bump version to 2.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chess.com",
-    version="2.1.2",
+    version="2.1.3",
     author="Artur Saradzhyan",
     author_email="sarartur.ruk@gmail.com",
     description="Python Wrapper for Chess.com API",
