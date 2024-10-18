7b592205286081494cdaba58aecf782c2cc3d488
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-10-17 20:07:54 -0400

    move more tests

--- tests/endpoint_test.py
+++ tests/endpoint_test.py
@@ -2,48 +2,6 @@ from chessdotcom import endpoints
 from tests.vcr import vcr
 
 
-@vcr.use_cassette("get_tournament_round_group_details(.yaml")
-def test_get_tournament_round_group_details():
-    response = endpoints.get_tournament_round_group_details(
-        "-33rd-chesscom-quick-knockouts-1401-1600", 1, 1
-    )
-
-    assert isinstance(response.json, dict)
-    assert isinstance(response.text, str)
-
-    tournament_round_group = response.tournament_round_group
-    assert isinstance(tournament_round_group.fair_play_removals, list)
-
-    for player in tournament_round_group.players:
-        assert isinstance(player.username, str)
-        assert isinstance(player.points, (float, int))
-        assert isinstance(player.is_advancing, bool)
-        assert isinstance(player.tie_break, (float, int))
-
-    for game in tournament_round_group.games:
-        assert isinstance(game.url, str)
-        assert isinstance(game.pgn, str)
-        assert isinstance(game.time_control, str)
-        assert isinstance(game.end_time, int)
-        assert isinstance(game.rated, bool)
-        assert isinstance(game.fen, str)
-        assert isinstance(game.start_time, int)
-        assert isinstance(game.time_class, str)
-        assert isinstance(game.rules, str)
-
-        assert isinstance(game.white.rating, int)
-        assert isinstance(game.white.result, str)
-        assert isinstance(game.white.id, str)
-        assert isinstance(game.white.username, str)
-        assert isinstance(game.white.uuid, str)
-
-        assert isinstance(game.black.rating, int)
-        assert isinstance(game.black.result, str)
-        assert isinstance(game.black.id, str)
-        assert isinstance(game.black.username, str)
-        assert isinstance(game.black.uuid, str)
-
-
 @vcr.use_cassette("get_team_match.yaml")
 def test_get_team_match():
     response = endpoints.get_team_match(12803)
