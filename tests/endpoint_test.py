0e52c0ac7973f6612e496dd9b4deed00bcd3cdda
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-10-17 20:03:53 -0400

    move more tests

--- tests/endpoint_test.py
+++ tests/endpoint_test.py
@@ -2,49 +2,6 @@ from chessdotcom import endpoints
 from tests.vcr import vcr
 
 
-@vcr.use_cassette("get_tournament_details.yaml")
-def test_get_tournament_details():
-    response = endpoints.get_tournament_details(
-        "-33rd-chesscom-quick-knockouts-1401-1600"
-    )
-
-    assert isinstance(response.json, dict)
-    assert isinstance(response.text, str)
-
-    tournament = response.tournament
-
-    assert isinstance(tournament.name, str)
-    assert isinstance(tournament.url, str)
-    assert isinstance(tournament.description, str)
-    assert isinstance(tournament.creator, str)
-    assert isinstance(tournament.status, str)
-    assert isinstance(tournament.finish_time, int)
-    assert isinstance(tournament.settings.type, str)
-    assert isinstance(tournament.settings.rules, str)
-    assert isinstance(tournament.settings.is_rated, bool)
-    assert isinstance(tournament.settings.is_official, bool)
-    assert isinstance(tournament.settings.is_invite_only, bool)
-    assert isinstance(tournament.settings.min_rating, int)
-    assert isinstance(tournament.settings.max_rating, int)
-    assert isinstance(tournament.settings.initial_group_size, int)
-    assert isinstance(tournament.settings.user_advance_count, int)
-    assert isinstance(tournament.settings.use_tiebreak, bool)
-    assert isinstance(tournament.settings.allow_vacation, bool)
-    assert isinstance(tournament.settings.winner_places, int)
-    assert isinstance(tournament.settings.registered_user_count, int)
-    assert isinstance(tournament.settings.games_per_opponent, int)
-    assert isinstance(tournament.settings.total_rounds, int)
-    assert isinstance(tournament.settings.concurrent_games_per_opponent, int)
-    assert isinstance(tournament.settings.time_class, str)
-    assert isinstance(tournament.settings.time_control, str)
-
-    for player in tournament.players:
-        assert isinstance(player.username, str)
-        assert isinstance(player.status, str)
-
-    assert all(isinstance(round, str) for round in tournament.rounds)
-
-
 @vcr.use_cassette("get_tournament_round.yaml")
 def test_get_tournament_round():
     response = endpoints.get_tournament_round(
