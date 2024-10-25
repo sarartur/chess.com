2f63f71f6c1cb16bd52a044ac2eb3f36de22200f
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-10-24 23:23:59 -0400

    add .readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Artur Saradzhyan"
 author = "Artur Saradzhyan"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.2"
+release = "3.0.2"
 
 
 # -- General configuration ---------------------------------------------------
