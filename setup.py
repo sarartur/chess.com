9052463e138dab4db53f071657a78163de0cec7f
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-10-25 00:11:01 -0400

    only lock major versions

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requires = [
+    "aiohttp>=3", 
+    "requests>=2",
+]
+
 setuptools.setup(
     name="chess.com",
-    version="3.0.2",
+    version="3.0.3",
     author="Artur Saradzhyan",
     author_email="sarartur.ruk@gmail.com",
     description="Python Wrapper for Chess.com API",
@@ -13,8 +18,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sarartur/chess.com",
     packages=setuptools.find_packages(),
-    install_requires=["aiohttp==3.10.2", "requests==2.28.0"],
-    setup_requires=["aiohttp==3.10.2", "requests==2.28.0"],
+    install_requires=requires,
+    setup_requires=requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
