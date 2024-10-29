cc9fedff04295c06e564b76963e6511b211c28c3
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-10-29 18:42:04 -0400

    bump version of docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Artur Saradzhyan"
 author = "Artur Saradzhyan"
 
 # The full version, including alpha/beta/rc tags
-release = "3.0.2"
+release = "3.5.0"
 
 
 # -- General configuration ---------------------------------------------------
