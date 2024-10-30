63c2a27ed2a674e641d13ddda95fb6c5d16ea681
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-10-30 16:01:01 -0400

    add doc string

--- chessdotcom/endpoints/player_games_by_month_pgn.py
+++ chessdotcom/endpoints/player_games_by_month_pgn.py
@@ -15,14 +15,14 @@ def get_player_games_by_month_pgn(
     datetime_obj: Optional[Union[datetime, None]] = None,
     tts=0,
     **request_options,
-) -> ChessDotComResponse:
+) -> "GetPlayerGamesByMonthResponsePgn":
     """
     :param username: username of the player.
     :param year: the year (yyyy).
     :param month: the month (mm).
     :param date: datetime.datetime of the month. Can be passed in instead of month
                     and year parameters.
-    :returns: ``ChessDotComResponse`` object containing
+    :returns: :obj:`GetPlayerGamesByMonthResponsePgn` object containing
                 standard multi-game format PGN containing all games for a month.
     """
     yyyy, mm = resolve_date(year, month, datetime_obj)
@@ -44,6 +44,12 @@ class ResponseBuilder(ResponseBuilder):
 
 
 class GetPlayerGamesByMonthResponsePgn(ChessDotComResponse):
+    """
+    :ivar json: The JSON response from the API.
+    :ivar text: The text response from the API.
+    :ivar pgn: :obj:`Pgn`: contains PGN data.
+    """
+
     def __init__(self, json, text, pgn):
         self.json = json
         self.text = text
@@ -52,5 +58,10 @@ class GetPlayerGamesByMonthResponsePgn(ChessDotComResponse):
 
 @dataclass(repr=True)
 class Pgn(object):
+    """
+    :ivar data: The PGN (Portable Game Notation) string representing the chess game.
+    :ivar pgn: Same as `data`, keeps backwards compatibility.
+    """
+
     pgn: Optional[str]
     data: Optional[str]
