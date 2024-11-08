daaa0ac2626b7e57c8e45608d0d96e99f01872e2
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2024-11-07 21:11:44 -0500

    bump minor

--- chessdotcom/__init__.py
+++ chessdotcom/__init__.py
@@ -32,4 +32,4 @@ from .endpoints import (
 from .errors import ChessDotComClientError, ChessDotComError
 from .response_builder import ChessDotComResponse
 
-__version__ = "3.6.0"
+__version__ = "3.7.0"
