36897e5acf8d2992740f6743ab790878c1522768
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2025-01-12 21:58:23 -0500

    create customer response builder

--- chessdotcom/endpoints/club_matches.py
+++ chessdotcom/endpoints/club_matches.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass
+from typing import Optional
+
 from ..client import Client, Resource
-from ..response_builder import ChessDotComResponse
+from ..response_builder import ChessDotComResponse, ResponseBuilder
 
 
 @Client.endpoint
@@ -12,6 +15,65 @@ def get_club_matches(url_id: str, tts=0, **request_options) -> ChessDotComRespon
     return Resource(
         uri=f"/club/{url_id}/matches",
         tts=tts,
-        top_level_attribute="matches",
         request_options=request_options,
+        response_builder=ResponseBuilder(),
     )
+
+
+class ResponseBuilder(ResponseBuilder):
+    def build(self, text):
+        data = self.serializer.deserialize(text)
+
+        return GetClubMatchesResponse(
+            json={"matches": data}, text=text, matches=self._build_club_matches(data)
+        )
+
+    def _build_club_matches(self, data):
+        return ClubMatches(
+            finished=self._build_matches(data.get("finished", [])),
+            in_progress=self._build_matches(data.get("in_progress", [])),
+            registered=self._build_matches(data.get("registered", [])),
+        )
+
+    def _build_matches(self, data):
+        return [
+            ClubMatch(
+                name=d.get("name"),
+                id=d.get("@id"),
+                opponent=d.get("opponent"),
+                start_time=d.get("start_time"),
+                time_class=d.get("time_class"),
+                result=d.get("result"),
+            )
+            for d in data
+        ]
+
+
+class GetClubMatchesResponse(ChessDotComResponse):
+    """
+    :ivar matches: Holds the list of daily and club matches.
+    :ivar json: The JSON response from the API.
+    :ivar text: The raw text response from the API.
+    """
+
+    def __init__(self, json, text, matches):
+        self.json = json
+        self.text = text
+        self.matches = matches
+
+
+@dataclass(repr=True)
+class ClubMatches(object):
+    finished: list
+    in_progress: list
+    registered: list
+
+
+@dataclass(repr=True)
+class ClubMatch(object):
+    name: Optional[str]
+    id: Optional[str]
+    opponent: Optional[str]
+    start_time: Optional[int]
+    time_class: Optional[str]
+    result: Optional[str]
