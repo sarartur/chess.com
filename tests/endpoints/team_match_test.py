fd3384d8e89687ba83eff729523bc40027e342db
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2025-01-14 10:48:36 -0500

    add expectations to test

--- tests/endpoints/team_match_test.py
+++ tests/endpoints/team_match_test.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from tests.vcr import vcr
@@ -22,9 +24,24 @@ async def test_with_async_client(async_client):
     validate_response(response)
 
 
-def validate_response(response):
+@vcr.use_cassette("get_team_match.yaml")
+@patch("chessdotcom.response_builder.Serializer.deserialize")
+def test_empty_data(deserialize, client):
+    deserialize.return_value = {}
+    response = client.get_team_match(match_id=12803)
+
+    validate_response_structure(response)
+
+
+def validate_response_structure(response):
     assert isinstance(response.json, dict)
     assert isinstance(response.text, str)
+
+
+def validate_response(response):
+    validate_response_structure(response)
+
+    assert response.json.get("match") is not None
 
     match = response.match
 
