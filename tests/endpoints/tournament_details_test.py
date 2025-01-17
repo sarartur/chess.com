6b07f7b71bafb644cdeb2f45013c7cdf2916b5fd
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2025-01-17 09:38:45 -0500

    amend test

--- tests/endpoints/tournament_details_test.py
+++ tests/endpoints/tournament_details_test.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from tests.vcr import vcr
@@ -28,9 +30,26 @@ async def test_with_async_client(async_client):
     validate_response(response)
 
 
-def validate_response(response):
+@vcr.use_cassette("get_tournament_details.yaml")
+@patch("chessdotcom.response_builder.Serializer.deserialize")
+def test_empty_data(deserialize, client):
+    deserialize.return_value = {}
+    response = client.get_tournament_details(
+        url_id="-33rd-chesscom-quick-knockouts-1401-1600"
+    )
+
+    validate_response_structure(response)
+
+
+def validate_response_structure(response):
     assert isinstance(response.json, dict)
     assert isinstance(response.text, str)
+
+
+def validate_response(response):
+    validate_response_structure(response)
+
+    assert response.json.get("tournament") is not None
 
     tournament = response.tournament
 
