ded43f6cbbea2ef14bf1edff844b74dec30cdf30
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2025-01-18 10:02:35 -0500

    rename and amend test

--- tests/endpoints/player_team_matches_test.py
+++ tests/endpoints/player_team_matches_test.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from tests.vcr import vcr
@@ -22,9 +24,24 @@ async def test_with_async_client(async_client):
     validate_response(response)
 
 
-def validate_response(response):
+@vcr.use_cassette("get_player_team_matches.yaml")
+@patch("chessdotcom.response_builder.Serializer.deserialize")
+def test_empty_data(deserialize, client):
+    deserialize.return_value = {}
+    response = client.get_player_team_matches(username="akshayraj_kore")
+
+    validate_response_structure(response)
+
+
+def validate_response_structure(response):
     assert isinstance(response.json, dict)
     assert isinstance(response.text, str)
+
+
+def validate_response(response):
+    validate_response_structure(response)
+
+    assert response.json.get("matches") is not None
 
     matches = response.matches
     for match in matches.finished:
