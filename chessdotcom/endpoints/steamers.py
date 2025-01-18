3c49db235c9773c99566f2c7eed5b0cad0c8d7db
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2025-01-17 21:46:00 -0500

    adjust test

--- chessdotcom/endpoints/steamers.py
+++ chessdotcom/endpoints/steamers.py
@@ -6,7 +6,7 @@ from ..response_builder import ChessDotComResponse
 def get_streamers(tts=0, **request_options) -> ChessDotComResponse:
     """
     :param tts: the time the client will wait before making the first request.
-    :returns: ``ChessDotComResponse`` object containing
+    :returns: :obj:`ChessDotComResponse` object containing
                 information about Chess.com streamers.
     """
     return Resource(uri="/streamers", tts=tts, request_options=request_options)
