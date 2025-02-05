245b7732207a78e052b11b4ab2f14216b8735d03
Artur Saradzhyan <artur.saradzhyan@simplybusiness.com>
2025-02-05 10:48:10 -0500

    bump patch

--- chessdotcom/__init__.py
+++ chessdotcom/__init__.py
@@ -32,4 +32,4 @@ from .endpoints import (
 from .errors import ChessDotComClientError, ChessDotComError
 from .response_builder import ChessDotComResponse
 
-__version__ = "3.11.0"
+__version__ = "3.11.1"
